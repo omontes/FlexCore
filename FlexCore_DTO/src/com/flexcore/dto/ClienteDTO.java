120400d07429516fceacfe9f15a4b9af0e310ef8
omontes <pulgamontes@gmail.com>
2014-09-23 21:20:54 -0600

    Se agrego el proyecto de DTOs

---
+++ FlexCore_DTO/src/com/flexcore/dto/ClienteDTO.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.flexcore.dto;
+
+/**
+ *
+ * @author Oscar Montes
+ */
+public class ClienteDTO {
+    private int idCliente;
+
+    /**
+     * @return the idCliente
+     */
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * @param idCliente the idCliente to set
+     */
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+}
